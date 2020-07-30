7fc601882891c95607147e67c369a00dda7a6db1
uturuncoglu <turuncu@ucar.edu>
2020-07-30 14:02:59 -0600

    let ESMF calculate grid area internally

--- NUOPC/hycom_read_latlon.F90
+++ NUOPC/hycom_read_latlon.F90
@@ -3,14 +3,11 @@
 
       contains
 
-      subroutine get_coord(lat_p,lon_p,area_p, &
-                           lat_q,lon_q,area_q, &
-                           itdm,jtdm,rc)
+      subroutine get_coord(lat_p,lon_p,lat_q,lon_q,itdm,jtdm,rc)
       implicit none
 
       real, dimension(itdm,jtdm), intent(inout) :: lat_p, lon_p
       real, dimension(itdm,jtdm), intent(inout) :: lat_q, lon_q
-      real, dimension(itdm,jtdm), intent(inout) :: area_p, area_q
       integer, intent(out) :: rc
       real*4, dimension(itdm,jtdm) :: tmp1, tmp2
       real*4, allocatable :: pad(:)
@@ -82,54 +79,14 @@
         enddo
       enddo
 
-      print *,'readHycomLatLon,plat, min,max=', &
+      print *,'get_coord, plat, min, max=', &
           minval(lat_p),maxval(lat_p)
-      print *,'readHycomLatLon,plon, min,max=', &
+      print *,'get_coord, plon, min, max=', &
           minval(lon_p),maxval(lon_p)
-      print *,'readHycomLatLon,qlat, min,max=', &
+      print *,'get_coord, qlat, min, max=', &
           minval(lat_q),maxval(lat_q)
-      print *,'readHycomLatLon,qlon, min,max=', &
+      print *,'get_coord, qlon, min, max=', &
           minval(lon_q),maxval(lon_q)
-
-      read(11,rec=10,iostat=ios) tmp1
-      if (ios.ne.0) then
-        print *,"error in reading regional.grid.a, pscx"
-        call exit(3)
-      endif
-
-      read(11,rec=11,iostat=ios) tmp2
-      if (ios.ne.0) then
-        print *,"error in reading regional.grid.a, pscy"
-        call exit(3)
-      endif
-
-      print *,'readHycomLatLon,pscx, min,max=', &
-          minval(tmp1),maxval(tmp1)
-      print *,'readHycomLatLon,pscy, min,max=', &
-          minval(tmp2),maxval(tmp2)
-
-      ! calculate area on p
-      area_p = tmp1*tmp2
-
-      read(11,rec=12,iostat=ios) tmp1
-      if (ios.ne.0) then
-        print *,"error in reading regional.grid.a, pscx"
-        call exit(3)
-      endif
-
-      read(11,rec=13,iostat=ios) tmp2
-      if (ios.ne.0) then
-        print *,"error in reading regional.grid.a, qscy"
-        call exit(3)
-      endif
-
-      print *,'readHycomLatLon,qscx, min,max=', &
-          minval(tmp1),maxval(tmp1)
-      print *,'readHycomLatLon,qscy, min,max=', &
-          minval(tmp2),maxval(tmp2)
-
-      ! calculate area on q
-      area_q = tmp1*tmp2
 
       ! deallocate temporary arrays
       if (allocated(pad)) deallocate(pad)
